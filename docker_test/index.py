945492c475780ee4042a9e835d1fe6a482b22886
MikhailEvt <mikhail.mi98@yandex.ru>
2022-01-20 17:43:24 +0300

    master

--- docker_test/index.py
+++ docker_test/index.py
@@ -1,2 +1,6 @@
 
-print('Hello Python!')
+print("!!")
+tf=open('./data/123.txt','a+')
+print(tf.read())
+tf.write("Hello Python")
+tf.close()
---
+++ docker_test/mfile.ps1
@@ -0,0 +1,2 @@
+docker run -v "C:\Users\mello\source\repos\JCoursesTest\docker_test\:/app" -v logs:/app/data --rm ftest
+
